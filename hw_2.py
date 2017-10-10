0e65ff782902eb011f39cb2e5a366b5eb412d15c
Allison Peroutka <aperoutka@uiowa.edu>
2017-10-10 16:01:45 -0500

    files

---
+++ hw_2.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+import pylab
+
+#find the current hour
+from datetime import datetime
+now = datetime.now()
+Lt = now.hour
+
+#use the latitude for Omaha,NE as given
+latitude = 41.15
+
+global s_0
+s_0 = 1368.0
+
+feb = 2452672.0
+may = 2452761.0
+aug = 2452853.0
+nov = 2452945.0
+
+if Lt == 12:
+    for
+
+def solarIrradiance(day, theta):
+    global s_0
+    sun_earth_ratio = 1.0 + 0.034 * np.cos(((day-3.0)/(365.0))*2*np.pi)
+    declination_angle = −23.45◦×cos((day+ 10.)/365×2*np.pi)
+    hour_angle = (2*np.pi/(24.×3600))×(Lt−12.×3600.)
+    cos_theta = np.cos(latitude)*np.cos(declination_angle)*np.cos(hour_angle) + np.sin(declinations_angle)*np.sin(latitude)
+    f = s_0*sun_earth_ratio*cos_theta
+    return f
+
+febX = []
+febY = []
+
+mayX = []
+mayY = []
+
+augX = []
+augY = []
+
+novX = []
+novY = []
+
+const = 4000.0
+
+for i in range(0,int(const)):
+    febX.append(i/const)
+    febY.append(solarIrradiance(feb,(i/const*np.pi)))
+    
+    mayX.append(i/const)
+    mayY.append(solarIrradiance(may,(i/const*np.pi)))
+    
+    augX.append(i/const)
+    augY.append(solarIrradiance(aug,(i/const*np.pi)))
+    
+    novX.append(i/const)
+    novY.append(solarIrradiance(nov,(i/const*np.pi)))
+
+pylab.plot(febX,febY,'b--',mayX,mayY,'y',augX,augY,'g--',novX,novY,'r')
+pylab.legend(["Feb","may","Aug","Nov"])
+pylab.xlim(-0.01,1.01)
+pylab.ylim(-1500,1500)
+pylab.show()
+
+print('done')
--- hw_problem_4.py
+++ hw_problem_4.py
@@ -18,21 +18,41 @@ nov = 2452945.0
 
 def solarIrradiance(day, theta):
     global s_0
-    sun_earth_ratio = 1.0 + 0.034 * np.cos((day-3.0)/(365.0)*np.pi)
+    sun_earth_ratio = 1.0 + 0.034 * np.cos(((day-3.0)/(365.0))*2*np.pi)
     f = s_0 * sun_earth_ratio*np.cos(theta)
     return f
 
-valsX = []
-valsY = []
+febX = []
+febY = []
 
-for i in range(0,100):
-    valsX.append(i/100.0)
-    valsY.append(solarIrradiance(feb,(i/100.0*np.pi)))
+mayX = []
+mayY = []
 
-pylab.plot(valsX,valsY,'r:')
+augX = []
+augY = []
 
-pylab.xlim(-0.25,1.25)
-pylab.ylim(-2000,2000)
+novX = []
+novY = []
+
+const = 4000.0
+
+for i in range(0,int(const)):
+    febX.append(i/const)
+    febY.append(solarIrradiance(feb,(i/const*np.pi)))
+    
+    mayX.append(i/const)
+    mayY.append(solarIrradiance(may,(i/const*np.pi)))
+    
+    augX.append(i/const)
+    augY.append(solarIrradiance(aug,(i/const*np.pi)))
+    
+    novX.append(i/const)
+    novY.append(solarIrradiance(nov,(i/const*np.pi)))
+
+pylab.plot(febX,febY,'b--',mayX,mayY,'y',augX,augY,'g--',novX,novY,'r')
+pylab.legend(["Feb","may","Aug","Nov"])
+pylab.xlim(-0.01,1.01)
+pylab.ylim(-1500,1500)
 pylab.show()
 
 print('done')
