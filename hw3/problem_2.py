6ee6d888f29ed40f9a3213b5326fa504726e863e
Nathaniel Le Sage <nataplane@gmail.com>
2018-01-12 17:15:36 -0600

    added comments, cleaned up some junk

--- hw3/problem_2.py
+++ hw3/problem_2.py
@@ -8,23 +8,22 @@ Created on Wed Jan 10 16:16:29 2018
 import numpy as np
 import pylab
 
-def planckFunction(wave, temp): # wavelength and temp (Kelvins)
-    if wave == 0.0:
+def planckFunction(wave, temp): # params are wavelength and temp (Kelvins)
+    if wave == 0.0: # don't divide by zero
         return 0.0
     k_b = np.float64(1.3806485279*10**-23) # boltzmann constant (joules/second)
     c = np.float64(2.997924580*10**8)      # speed of light (meters/second)
     h = np.float64(6.62607004081*10**-34)  # planck constant (joules/second)
     
+    # planck's law
     spectral_density = np.float64( (2*h*c**2) / ((wave**5)*(np.e**( (h*c)/(k_b*wave*temp) )-1)) )*(10**-6)
     return spectral_density 
-    # return ( (2*h)/(c**3) ) * ( (freq**3)/(np.e**( (h*freq)/(k_b*temp) ) -1 ) )
-    # return 2*h*(c**2)/((wave**5)*(np.exp(h*c/(k_b*wave*temp))-1))/1000.0/1000.0
     
-def wienDisplacement(temp):
-    wave_max = (2897.0)/temp
+def wienDisplacement(temp): # Wien's displacement law
+    wave_max = (2897.0)/temp # use Wien's displacement constant
     return wave_max
 
-def run():
+def run(): # draw the graph
     x = []
     sun = []
     earth = []
@@ -32,7 +31,7 @@ def run():
     wienDisp = []
     
     step = np.float(1)
-    for i in range(1, 100):
+    for i in range(1, 100): # do Planck's Law calculations
         x.append(i*step)
         sun.append((planckFunction(i*10**-6, 6000.0)))
         earth.append((planckFunction(i*10**-6, 300.0)))
@@ -44,8 +43,10 @@ def run():
     pylab.plot(x,sun,linewidth='2.5', label='6000 K')
     pylab.plot(x,earth,linewidth='2.5', label='300 K')
     pylab.plot(x,mars,linewidth='2.5',label='250 K')
-    pylab.plot(x,wienDisp,linewidth='2.5',label='Wien Displacement')
+    pylab.plot(x,wienDisp,linewidth='2.5',label='Wien Displacement') 
     pylab.xlabel('Wavelength (µm)')
     pylab.ylabel('Spectral radiance (W m$^{−2}$ µm$^{−1}$ sr$^{−1}$)')
     pylab.legend()
     pylab.show()
+    
+run()
--- hw3/problem_6.5.py
+++ hw3/problem_6.5.py
@@ -11,27 +11,29 @@ import numpy as np
 
 def run():
     x = []
-    wicky_woo = []
+    y = []
     
     for i in range(1, 1001):
         x.append(i/100.0)
-        wicky_woo.append((planckFunction(i*10**-8, 2850.0)))
+        y.append((planckFunction(i*10**-8, 2850.0)))
     
-    # pylab.ticklabel_format(axis='y', style='sci', scilimits=(-3,4))
-    pylab.plot(x,wicky_woo,linewidth='2.5', label='2850 K')
+    pylab.ticklabel_format(axis='y', style='sci', scilimits=(-3,4)) # label in scientific notation
+    pylab.plot(x,y,linewidth='2.5', label='2850 K')
     pylab.xlabel('Wavelength (µm)')
     pylab.ylabel('Spectral radiance (W m$^{−2}$ µm$^{−1}$ sr$^{−1}$)')
     pylab.legend()
     pylab.show()
 
-def area(a, b, temp, steps=2048.0): # use Simpson's rule to estimate area under curve
+def area(a, b, temp, steps=8192.0): # use Simpson's rule to estimate area under curve
     area = 0
-    h = (b-a)/steps
-    segs = np.arange(a,b,(b-a)/steps)
-    for i in range(len(segs)-1):
-        area += (h/6)*(planckFunction(segs[i], temp) + 4*planckFunction((segs[i+1]+segs[i])/2.0, temp) + planckFunction(segs[i+1], temp))
+    h = (b-a)/steps # calculate step size
+    segs = np.arange(a,b,(b-a)/steps) # get the x-value of each step
+    for i in range(len(segs)-1): # sum the area under the cubic polynomials
+        area += (h/6)*(planckFunction(segs[i], temp) +
+                 4*planckFunction((segs[i+1]+segs[i])/2.0, temp) +
+                 planckFunction(segs[i+1], temp))
     return area*10**6
 
 # print(area(4.0*10**-6,7.0*10**-6,2850))
-print(area(4.0*10**-6,7.0*10**-6,2850))
+print(area(4.0*10**-6,7.0*10**-6,2850,8192))
 run()
