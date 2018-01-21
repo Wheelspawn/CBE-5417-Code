8b16c4d39fa5b4c3678b3d36e6574c0af13ce817
Nathaniel Le Sage <nataplane@gmail.com>
2018-01-20 22:06:33 -0600

    problem 1 has a much improved plot

--- hw3/problem_1.py
+++ hw3/problem_1.py
@@ -5,9 +5,9 @@ Spyder Editor
 This is a temporary script file.
 """
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pylab
-
 
 #find the current hour
 from datetime import datetime
@@ -19,23 +19,27 @@ s_0 = 1368.0
 
 year_begin = 0 # january 1, 2012
 year_end = 	364 # december 31, 2012
-    
-def hourlySolarIrradiance(latitude, day, hour): # returns solar irradiance on given day, latitude and hour
+
+    # returns solar irradiance on given day, latitude and hour
+def hourlySolarIrradiance(latitude, day, hour):
     global s_0
     sun_earth_ratio = 1.0 + 0.034 * np.cos(((day-3.0)/(365.0))*2*np.pi)
     declination_angle = np.deg2rad(-23.45)*np.cos((day+ 10.)/365*2*np.pi)
     hour_angle = (2*np.pi/(24*3600))*(hour*3600.-12*3600.)
-    cos_theta = np.cos(latitude)*np.cos(declination_angle)*np.cos(hour_angle) + np.sin(declination_angle)*np.sin(latitude)
+    cos_theta = ( np.cos(latitude)*np.cos(declination_angle)*np.cos(hour_angle)
+                + np.sin(declination_angle)*np.sin(latitude) )
     f = s_0*sun_earth_ratio*cos_theta
     
     return 0 if f < 0 else f
     
-def averagedSolarIrradiance(day, latitude): # returns average solar irradiance on given day and latitude
+    # returns average solar irradiance on given day and latitude
+def averagedSolarIrradiance(day, latitude):
     avg = 0.0
-    for i in range(24): # hours
-        avg += hourlySolarIrradiance(np.deg2rad(latitude),day,i)
-    return avg/24.0
+    for i in range(12): # hours
+        avg += hourlySolarIrradiance(np.deg2rad(latitude),day,i*2)
+    return avg/12.0
 
+'''
 x = np.linspace(year_begin, year_end, 365) # x-axis from january 1, 2012 to december 31, 2012--day
 y = np.linspace(-90, 90, 180) # y-axis from -90 degrees to 90 degrees--latitude
 
@@ -52,7 +56,59 @@ manuallocations = [(20, 182),(40, 182),(60, 182),(80, 182),
 pylab.plt.clabel(CS, inline=1, fontsize=10, manual=manuallocations)
 pylab.xlabel('Latitude')
 pylab.ylabel('Days (of the year)')
+'''
 
-# m = Basemap()
-# cs = m.contour()
-# clabels = ax.clabel(CS)
+x = np.linspace(-90, 90, 180)
+y = np.linspace(year_begin, year_end, 365)
+
+X, Y = np.meshgrid(x, y) # set up the contour
+Z = np.zeros((np.size(x), np.size(y))) # calculate the z values
+W = np.zeros((np.size(x), np.size(y)))
+
+for i in range(np.size(x)):
+    for j in range(np.size(y)):
+        a = averagedSolarIrradiance(y[j], x[i])
+        if a == 0.0:
+            W[i,j] = 1
+        else:
+            W[i,j] = None
+        Z[i,j] = a
+
+pylab.close()
+
+ax = plt.subplot(111)
+
+center = 173.5
+label_bbox=dict(facecolor='white', edgecolor='none', pad=1.0)
+
+# CF = pylab.plt.contour(Z, 0, colors='black')
+pylab.plt.contourf(W, 10, colors='gray')
+CS = pylab.plt.contour( Z, 20, colors='black') # plot contour
+manuallocations = [(center, 20),(center, 40),(center, 60),(center, 80),
+                   (center, 100)]
+pylab.plt.clabel(CS, inline=0, fontsize=10, manual=manuallocations)
+
+pylab.title("Daily average insolation [W m$^{-2}$]")
+
+pylab.text(160,170,'24 hr light',backgroundcolor='white',bbox=label_bbox)
+pylab.text(160,10,'24 hr darkness')
+
+pylab.text(10,170,'24 hr darkness')
+pylab.text(310,170,'24 hr darkness')
+
+pylab.text(10,10,'24 hr light',backgroundcolor='white',bbox=label_bbox)
+pylab.text(310,10,'24 hr light',backgroundcolor='white',bbox=label_bbox)
+
+new_x_axis = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
+new_y_axis = ['SP', '60S', '30S', 'EQ', '30N', '60N', 'NP']
+
+months=[0,30,58,89,119,150,180,211,242,272,303,333,364]
+lats=[0,30,60,90,120,150,180]
+
+ax.set_xticks(months)
+ax.set_xticklabels(new_x_axis)
+
+ax.set_yticks(lats)
+ax.set_yticklabels(new_y_axis)
+
+pylab.show()
--- hw3/problem_2.py
+++ hw3/problem_2.py
@@ -8,17 +8,29 @@ Created on Wed Jan 10 16:16:29 2018
 import numpy as np
 import pylab
 
+global k_b, c, h, s_0
+k_b = np.float64(1.3806485279*10**-23) # boltzmann constant (joules/second)
+c = np.float64(2.997924580*10**8)      # speed of light (meters/second)
+h = np.float64(6.62607004081*10**-34)  # planck constant (joules/second)
+s_0 = 1370.0 # solar flux constant
+
 def planckFunction(wave, temp): # params are wavelength and temp (Kelvins)
+    global kk_b, c, h
+    
     if wave == 0.0: # don't divide by zero
         return 0.0
-    k_b = np.float64(1.3806485279*10**-23) # boltzmann constant (joules/second)
-    c = np.float64(2.997924580*10**8)      # speed of light (meters/second)
-    h = np.float64(6.62607004081*10**-34)  # planck constant (joules/second)
     
     # planck's law
-    spectral_density = np.float64( (2*h*c**2) / ((wave**5)*(np.e**( (h*c)/(k_b*wave*temp) )-1)) )*(10**-6)
-    return spectral_density 
+    spectral_density = np.float64( (2*h*c**2) / ((wave**5)*(np.e**( (h*c)/(k_b*wave*temp) )-1)) )
+    return spectral_density
+
+def planckInverse(wave, blackbody): # inverse planck function, returns temperature from wave+blackbody
+    global k_b, c, h
     
+def effectiveEmittingTemperature(distance, albedo):
+    global s_0
+    return ( s_0*distance*( 1-albedo )/( 4*5.67*10**-8 ) )**( 1/4 )
+        
 def wienDisplacement(temp): # Wien's displacement law
     wave_max = (2897.0)/temp # use Wien's displacement constant
     return wave_max
@@ -30,12 +42,11 @@ def run(): # draw the graph
     mars = []
     wienDisp = []
     
-    step = np.float(1)
-    for i in range(1, 100): # do Planck's Law calculations
-        x.append(i*step)
-        sun.append((planckFunction(i*10**-6, 6000.0)))
-        earth.append((planckFunction(i*10**-6, 300.0)))
-        mars.append((planckFunction(i*10**-6, 250.0)))
+    for i in range(1, 100000): # do Planck's Law calculations
+        x.append(i*0.01)
+        sun.append((planckFunction(i*10**-8, 6000.0)))
+        earth.append((planckFunction(i*10**-8, 300.0)))
+        mars.append((planckFunction(i*10**-8, 250.0)))
         wienDisp.append(wienDisplacement(i*60))
     
     pylab.xscale('log')
@@ -43,7 +54,7 @@ def run(): # draw the graph
     pylab.plot(x,sun,linewidth='2.5', label='6000 K')
     pylab.plot(x,earth,linewidth='2.5', label='300 K')
     pylab.plot(x,mars,linewidth='2.5',label='250 K')
-    pylab.plot(x,wienDisp,linewidth='2.5',label='Wien Displacement') 
+    pylab.plot(x,wienDisp,linewidth='2.5',label='Wien Displacement')
     pylab.xlabel('Wavelength (µm)')
     pylab.ylabel('Spectral radiance (W m$^{−2}$ µm$^{−1}$ sr$^{−1}$)')
     pylab.legend()
